4043acbf0a626269702d920ff35c92eeaec4a9f5
dukeseb <109305417+dukeseb@users.noreply.github.com>
2024-01-09 10:06:21 -0500

    changed ownership of www/$domain

--- webserver.sh
+++ webserver.sh
@@ -112,7 +112,7 @@ groupadd sftp
 useradd -g sftp -d /var/www/$domain -s /sbin/nologin $ftplogin
 echo -e "\n \nEnter password for SFTP / SSH login"
 passwd $ftplogin
-chown $ftplogin:sftp /var/www/$domain
+chown root:root /var/www/$domain
 #Append Write to file /etc/ssh/sshd_config
   # AllowGroups ssh sftp
   # Match Group sftp
