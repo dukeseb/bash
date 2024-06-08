0356025964710f6dce0a24f3c77ceba67c275802
dukeseb <109305417+dukeseb@users.noreply.github.com>
2024-06-08 14:32:48 -0400

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -26,6 +26,8 @@ echo -e "${green}\n \nWhat is the email associated with this domain?${clear}"
 read email
 
 #Install updates
+echo "Checking && Installing Updates"
+sleep 2
 apt update && apt upgrade -y
 
 #Install Apache Web Server
@@ -46,32 +48,23 @@ ufw allow 22
 #Setting up Virtual Host
 echo -e "${yellow}\n \nSetting up Virtual Host${clear}"
 sleep 2
-#write to file /etc/apache2/sites-available/$domain.conf
-#<VirtualHost *:80>
-#    ServerAdmin $email
-#    ServerName $domain
-#    ServerAlias www.$domain
-#    DocumentRoot /var/www/$domain
-#    ErrorLog ${APACHE_LOG_DIR}/error.log
-#    CustomLog ${APACHE_LOG_DIR}/access.log combined
-#</VirtualHost>
-echo -e "<VirtualHost *:80> \n  ServerAdmin $email \n  ServerName $domain \n  ServerAlias www.$domain \n  DocumentRoot /var/www/$domain \n  ErrorLog ${APACHE_LOG_DIR}/error.log \n  CustomLog ${APACHE_LOG_DIR}/access.log combined \n</VirtualHost>" >> /etc/apache2/sites-available/$domain.conf
+echo -e "<VirtualHost *:80> \n  ServerAdmin $email \n  ServerName $domain \n  ServerAlias www.$domain \n  DocumentRoot /var/www/$domain \n  ErrorLog ${APACHE_LOG_DIR}/error.log \n  CustomLog ${APACHE_LOG_DIR}/access.log combined \n  <IfModule mod_speling.c> \n    CheckSpelling On \n    CheckCaseOnly On \n  </IfModule> \n</VirtualHost>" >> /etc/apache2/sites-available/$domain.conf
 a2dissite 000-default.conf
 a2ensite $domain.conf
+a2enmod speling
 echo -e "${yellow}\n \nIs Virtual Host configuration syntax OK${clear}"
 sleep 3
 apache2ctl configtest
 sleep 3
-echo -e "${yellow}\n \nDisabling Web Indexes in apache2.conf${clear}"
+echo -e "${yellow}\n \nDisabling Web Indexes${clear}"
 sleep 3
 sed -i 's/Options Indexes FollowSymLinks/Options FollowSymLinks/g' /etc/apache2/apache2.conf
-sed -i '/<VirtualHost \*:80>/,/<\/VirtualHost>/s/^/    CheckCaseOnly on\n    CheckSpelling on\n/' /etc/apache2/$domain.conf
 systemctl restart apache2
 systemctl enable apache2
 
 #Install MariaDB
 while true; do
-read -p "${green}Do you want to install MariaDB? (y/n)${clear} " yn
+read -p "${green}\n \nDo you want to install MariaDB? (y/n)${clear} " yn
 case $yn in
   [yY] ) echo -e "${yellow}\n \nInstalling MariaDB${clear}";
       sleep 2;
@@ -90,14 +83,14 @@ apt install -y apt-transport-https lsb-release ca-certificates wget
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list 
 apt update
-apt -y install php$phpversion #php-xml php-curl <---- need to figure this out for blog page
+apt -y install php$phpversion php$phpversion-xml php$phpversion-curl #<---- need to figure this out for blog page
 #apt-get install -y php$phpversion-cli php$phpversion-common php$phpversion-mysql php$phpversion-zip php$phpversion-gd php$phpversion-mbstring php$phpversion-curl php$phpversion-xml php$phpversion-bcmath
 a2enmod php
 systemctl restart apache2
 
 
 #Install SFTP
-echo -e "${yellow}\n \nSetting up SSH / SFTP${clear}"
+echo -e "${yellow}\n \nSetting up SFTP${clear}"
 sleep 2
 echo -e "${green}What is the username for SFTP Access?${clear}"
 read ftplogin
@@ -106,7 +99,7 @@ echo -e "${magenta}Creating SFTP User $ftplogin and Assigning User to $domain${c
 sleep 2
 groupadd sftpusers
 useradd -m -g sftpusers -s /bin/false $ftplogin
-echo -e "${green}\n \nEnter password for SFTP / SSH login${clear}"
+echo -e "${green}\n \nEnter password for SFTP login${clear}"
 passwd $ftplogin
 sed -i 's|Subsystem\s\s*sftp\s\s*/usr/lib/openssh/sftp-server|Subsystem sftp internal-sftp|' /etc/ssh/sshd_config
 echo -e "\nMatch Group sftpusers \n \tX11Forwarding no \n \tAllowTcpForwarding no \n \tChrootDirectory /var/www/ \n \tForceCommand internal-sftp" >> /etc/ssh/sshd_config
